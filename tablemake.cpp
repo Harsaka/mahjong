fb64bd393d7acd822e58fb2f8535d59063cb33b7
Harsaka <redder710@gmail.com>
2019-10-24 10:32:20 +0900

    add check_succes()

--- tablemake.cpp
+++ tablemake.cpp
@@ -40,6 +40,7 @@ int main(){
     vvll final_state = climbing(first_state); 
     vvll final_match_set = matchcalc(final_state); // final_match_set[a][b] : a と　b の直接対決の回数.
     
+    check_success(final_state);
     print_table(final_state);
     cout << "-----------------------------" << endl;
     print_matching(final_match_set);
@@ -66,33 +67,33 @@ vvll make_absent(){
 }
 
 // 最初に乱数で適当に卓組を初期化する.
-vvll initialize(vvll &absent){
+vvll initialize(vvll &absent) {
     vvll first_state(m, vll(n));
     ll tmp;
     REP(i, m){
         vll count(3); // 卓に座っている人数.
         vll used(3); // 満卓判定. 満卓なら座れない.
         REP(j, n){
-            if(absent[i][j]){
+            if (absent[i][j]) {
                 first_state[i][j] = -1;
                 continue;
             }
-            if(i == 4){
+            if (i == 4) {
                 tmp = rand() % 2;
-                if(used[tmp]){
+                if (used[tmp]) {
                     tmp = (tmp + 1) % 2;
                 }
             }
-            else{
+            else {
                 tmp = rand() % 3;
-                while(used[tmp]){
+                while (used[tmp]) {
                     tmp = (tmp + 1) % 3;
                 }
             }
             first_state[i][j] = tmp;
             count[tmp]++;
             REP(k, 3){
-                if(count[k] == 4){
+                if (count[k] == 4) {
                     used[k] = 1;
                 }
             }
@@ -102,13 +103,13 @@ vvll initialize(vvll &absent){
 }
 
 // 卓組の情報 tablestate から直接対決の回数を計算しその vector を返す.
-vvll matchcalc(vvll &table_state){
+vvll matchcalc(vvll &table_state) {
     vvll res(n, vll(n));
     REP(i, m){
         REP(a, n){
             REP(b, n){
-                if(b <= a || table_state[i][a] == -1) continue;
-                if(table_state[i][a] == table_state[i][b]){
+                if (b <= a || table_state[i][a] == -1) continue;
+                if (table_state[i][a] == table_state[i][b]) {
                     res[a][b]++;
                     res[b][a]++;
                 }
@@ -130,7 +131,7 @@ void check_success(vvll &table_state) {
 }
 
 // 卓組の出力
-void print_table(vvll &table_state){
+void print_table(vvll &table_state) {
     cout << "Which table each player is sitting at." << endl;
     cout << "    ";
     REP(i, m){
@@ -151,7 +152,7 @@ void print_table(vvll &table_state){
 }
 
 // 対戦回数の出力
-void print_matching(vvll &match_set){
+void print_matching(vvll &match_set) {
     REP(a, n){
         REP(b, n){
             cout << match_set[a][b] << " ";
@@ -162,12 +163,12 @@ void print_matching(vvll &match_set){
 }
 
 // 同卓回数が 3 を超えた分を計算
-ll violation(vvll &table_state){
+ll violation(vvll &table_state) {
     ll res = 0;
     vvll match = matchcalc(table_state);
     REP(a, n){
         REP(b, n){
-            if(match[a][b] > 3){
+            if (match[a][b] > 3) {
                 res += match[a][b] - 3;
             }
         }
@@ -176,7 +177,7 @@ ll violation(vvll &table_state){
 }
 
 // 現在の状態から violation が小さくなるような変更を行う.
-vvll update(vvll &current_state){
+vvll update(vvll &current_state) {
     vvll curmatch = matchcalc(current_state);
     vvll next_state = current_state;
     ll provisional_violation = violation(current_state);
@@ -185,26 +186,26 @@ vvll update(vvll &current_state){
         vll ma, mi; // a とマッチングしすぎている選手とあまりマッチングしていない選手の添え字集合
         REP(idx, n){
             REP(x, 6){
-                if(curmatch[a][idx] == 9 - x){
+                if (curmatch[a][idx] == 9 - x) {
                     ma.push_back(idx);
                 }
             }
         }
         REP(idx, n){
             REP(x, 3){
-                if(curmatch[a][idx] == x){
+                if (curmatch[a][idx] == x) {
                     mi.push_back(idx);
                 }
             }
         }
-        for(auto p : ma){
-            for(auto q : mi){
+        for (auto p : ma) {
+            for (auto q : mi) {
                 REP(i, m){
-                    if(current_state[i][a] == -1 || current_state[i][p] == -1 || current_state[i][q] == -1) continue;
-                    if(current_state[i][a] == current_state[i][p] && current_state[i][a] != current_state[i][q]){
+                    if (current_state[i][a] == -1 || current_state[i][p] == -1 || current_state[i][q] == -1) continue;
+                    if (current_state[i][a] == current_state[i][p] && current_state[i][a] != current_state[i][q]) {
                         next_state[i][p] = next_state[i][q];
                         next_state[i][q] = next_state[i][p];
-                        if(violation(next_state) < provisional_violation){
+                        if (violation(next_state) < provisional_violation) {
                             provisional_violation = violation(next_state);
                             res = next_state;
                         }
@@ -219,12 +220,12 @@ vvll update(vvll &current_state){
 }
 
 // 初期状態に対して update の実行
-vvll climbing(vvll &first_state){
+vvll climbing(vvll &first_state) {
     vvll currentstate = first_state;
     REP(cnt, 20){
         currentstate = update(currentstate);
         //cout << violation(currentstate) << endl;
-        if(violation(currentstate) == 0) break;
+        if (violation(currentstate) == 0) break;
     }
     return currentstate;
 }
